358cdf81bcd884e56466f6b3e827205d4449c8c1
Juan-David-Moreno-Suarez <juanmorsua@unisabana.edu.co>
2026-05-12 11:40:18 -0500

    Error fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const PORT = process.env.PORT || 3000;
 
 // Middlewares
 const sanitizeIds = require('../middlewares/sanitizeIds.js');
